4e12fac52c0103053bb4fe058c268fb081ef613b
Matthew Gitata <matthewgitata@gmail.com>
2023-03-16 09:33:00 +0300

    Start: Spring Web App Project.

---
+++ springwebapp/src/test/java/com/matthewgitata/demo/SpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matthewgitata.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
