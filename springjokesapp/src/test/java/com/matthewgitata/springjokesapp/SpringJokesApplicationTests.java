b32f4850458f592895b848b7fbc87779dd5fd071
Matthew Gitata <matthewgitata@gmail.com>
2023-03-17 11:00:33 +0300

    Start: Initial add from Spring Initializr.

---
+++ springjokesapp/src/test/java/com/matthewgitata/springjokesapp/SpringJokesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matthewgitata.springjokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJokesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
