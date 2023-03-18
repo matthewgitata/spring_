9cb433e94ffe3455166dbb79a47a2fff58c13584
Matthew Gitata <matthewgitata@gmail.com>
2023-03-18 13:27:00 +0300

    Added properties, to Owner.

--- petclinic/pet-clinic-data/src/main/java/com/matthewgitata/petclinic/model/Owner.java
+++ petclinic/pet-clinic-data/src/main/java/com/matthewgitata/petclinic/model/Owner.java
@@ -9,5 +9,40 @@ import java.util.Set;
  * created by @matthewgitata on 17/03/2023
  */
 public class Owner extends Person {
+    private String address;
+    private String city;
+    private String telephone;
     private Set<Pet> pets;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
