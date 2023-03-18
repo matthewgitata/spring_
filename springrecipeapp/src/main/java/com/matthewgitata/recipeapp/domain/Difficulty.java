f66d8d5fc6dc28896e0daecae75bf94ee1f091fb
Matthew Gitata <matthewgitata@gmail.com>
2023-03-18 23:49:00 +0300

    Add Difficulty enumeration.

---
+++ springrecipeapp/src/main/java/com/matthewgitata/recipeapp/domain/Difficulty.java
@@ -0,0 +1,8 @@
+package com.matthewgitata.recipeapp.domain;
+
+/**
+ * created by @matthewgitata on 18/03/2023
+ */
+public enum Difficulty {
+    EASY, MODERATE, HARD
+}
--- springrecipeapp/src/main/java/com/matthewgitata/recipeapp/domain/Recipe.java
+++ springrecipeapp/src/main/java/com/matthewgitata/recipeapp/domain/Recipe.java
@@ -1,5 +1,7 @@
 package com.matthewgitata.recipeapp.domain;
 
+import sun.jvm.hotspot.debugger.cdbg.EnumType;
+
 import java.util.Set;
 
 /**
@@ -17,8 +19,8 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    // TODO: add
-    // private Difficulty difficulty;
+    @Enumerated(value= EnumType.STRING)
+    private Difficulty difficulty;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
     @Lob
@@ -97,6 +99,14 @@ public class Recipe {
 
     public void setImage(Byte[] image) {
         this.image = image;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 
     public Notes getNotes() {
