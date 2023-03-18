6206d87fcd5620b5e61bb89088a623146b4a7cbc
Matthew Gitata <matthewgitata@gmail.com>
2023-03-19 00:46:00 +0300

    Add Data Repositories for Recipe, Category, and UnitOfMeasure.

---
+++ springrecipeapp/src/main/java/com/matthewgitata/recipeapp/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.matthewgitata.recipeapp.repositories;
+
+import com.matthewgitata.recipeapp.domain.Category;
+
+/**
+ * created by @matthewgitata on 18/03/2023
+ */
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+}
---
+++ springrecipeapp/src/main/java/com/matthewgitata/recipeapp/repositories/UnitOfMeasureRepository.java
@@ -0,0 +1,9 @@
+package com.matthewgitata.recipeapp.repositories;
+
+import com.matthewgitata.recipeapp.domain.UnitOfMeasure;
+
+/**
+ * created by @matthewgitata on 18/03/2023
+ */
+public class UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long>{
+}
