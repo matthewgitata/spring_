613b73b9bb68fa9795398b4afdf8d4d8af9b4f42
Matthew Gitata <matthewgitata@gmail.com>
2023-03-19 00:27:00 +0300

    Add Category entity, set up recipe-category relationship.

--- springrecipeapp/src/main/java/com/matthewgitata/recipeapp/domain/Recipe.java
+++ springrecipeapp/src/main/java/com/matthewgitata/recipeapp/domain/Recipe.java
@@ -19,7 +19,7 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    @Enumerated(value= EnumType.STRING)
+    @Enumerated(value = EnumType.STRING)
     private Difficulty difficulty;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
@@ -28,6 +28,12 @@ public class Recipe {
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
 
     public Long getId() {
         return id;
@@ -93,6 +99,14 @@ public class Recipe {
         this.directions = directions;
     }
 
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
     public Byte[] getImage() {
         return image;
     }
@@ -115,5 +129,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
