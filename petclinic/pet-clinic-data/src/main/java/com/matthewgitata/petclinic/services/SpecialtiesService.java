1cca7e727e486a8c3f438997db0ac3e67342e1a7
Matthew Gitata <matthewgitata@gmail.com>
2023-03-18 15:19:00 +0300

    Add Specialties Map Impl.

---
+++ petclinic/pet-clinic-data/src/main/java/com/matthewgitata/petclinic/services/SpecialtiesService.java
@@ -0,0 +1,9 @@
+package com.matthewgitata.petclinic.services;
+
+import com.matthewgitata.petclinic.model.Specialty;
+
+/**
+ * created by @matthewgitata on 18/03/2023
+ */
+public interface SpecialtiesService extends CrudService<Specialty, Long>{
+}
