8c7c34209fe7e627978aa8efabb88465df314906
Matthew Gitata <matthewgitata@gmail.com>
2023-03-18 16:02:00 +0300

    Fixed Broken Links.

--- petclinic/pet-clinic-web/src/main/java/com/matthewgitata/petclinic/controllers/VetController.java
+++ petclinic/pet-clinic-web/src/main/java/com/matthewgitata/petclinic/controllers/VetController.java
@@ -12,7 +12,7 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
     public String listVets(Model mode) {
 
         model.addAttribute("vets", vetService.findAll());
