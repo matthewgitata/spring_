533d02e290b1aaf033a2ad4dfc902608115014b6
Matthew Gitata <matthewgitata@gmail.com>
2023-03-18 13:53:00 +0300

    Added PetTypes to bootstrap.

--- petclinic/pet-clinic-web/src/main/java/com/matthewgitata/petclinic/bootstrap/DataLoader.java
+++ petclinic/pet-clinic-web/src/main/java/com/matthewgitata/petclinic/bootstrap/DataLoader.java
@@ -8,10 +8,12 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     public DataLoader() {
@@ -19,6 +21,15 @@ public class DataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
