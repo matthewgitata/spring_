b2409460dc810801fbf94b1502b9d932dc2dae87
Matthew Gitata <matthewgitata@gmail.com>
2023-03-19 18:39:00 +0300

    Mockito Argument Capture.

--- springrecipeapp/src/test/java/com/matthewgitata/recipeapp/controllers/IndexControllerTest.java
+++ springrecipeapp/src/test/java/com/matthewgitata/recipeapp/controllers/IndexControllerTest.java
@@ -1,7 +1,9 @@
 package com.matthewgitata.recipeapp.controllers;
 
-
+import com.matthewgitata.recipeapp.domain.Recipe;
 import com.matthewgitata.recipeapp.service.RecipeService;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,9 +23,26 @@ class IndexControllerTest {
 
     @org.junit.jupiter.api.Test
     void getIndexPage() throws Exception {
+        // Given
+        Set<Recipe> recipes = new HashSet<Recipe>();
+        recipes.add(new Recipe());
+
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        recipes.add(recipe);
+
+        // When
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
         String viewName = controller.getIndexPage(model);
+
+        // then
         assertEquals("index", viewName);
         verify(recipeService, times(1)).getRecipes();
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
     }
 }
