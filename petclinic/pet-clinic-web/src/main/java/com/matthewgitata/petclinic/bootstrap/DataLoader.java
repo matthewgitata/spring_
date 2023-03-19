5747deb7c3d3d4429dad56f1709d23b494c6b067
Matthew Gitata <matthewgitata@gmail.com>
2023-03-19 12:33:00 +0300

    Implement JPA Visit Service, Adds Loader Data.

--- petclinic/pet-clinic-web/src/main/java/com/matthewgitata/petclinic/bootstrap/DataLoader.java
+++ petclinic/pet-clinic-web/src/main/java/com/matthewgitata/petclinic/bootstrap/DataLoader.java
@@ -12,12 +12,14 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialtyService specialtyService;
+    private final VisitService visitService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService, VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialtyService = specialtyService;
+        this.visitService = visitService;
     }
 
     public DataLoader() {
@@ -88,7 +90,14 @@ public class DataLoader implements CommandLineRunner {
 
         ownerService.save(owner2);
 
-        System.out.println("Loading Owners...");
+        Visit catVisit = new Visit();
+        catVisit.setPet(fionasCat);
+        catVisit.setDate(LocalDate.now());
+        catVisit.setDescription("Sneezy Kitty");
+
+        visitService.save(catVisit);
+
+        System.out.println("Loaded Owners...");
 
         Vet vet1 = new Vet();
         vet1.setFirstName("Sam");
@@ -104,6 +113,6 @@ public class DataLoader implements CommandLineRunner {
 
         vetService.save(vet2);
 
-        System.out.println("Loading Vets...");
+        System.out.println("Loaded Vets...");
     }
 }
