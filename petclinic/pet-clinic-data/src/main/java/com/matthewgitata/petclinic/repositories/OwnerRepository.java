21323b61c7b0f5b1201a4fd858f7eceabf5fed4f
Matthew Gitata <matthewgitata@gmail.com>
2023-03-19 10:52:00 +0300

    Add Spring Data JPA Repositories.

---
+++ petclinic/pet-clinic-data/src/main/java/com/matthewgitata/petclinic/repositories/OwnerRepository.java
@@ -0,0 +1,9 @@
+package com.matthewgitata.petclinic.repositories;
+
+import com.matthewgitata.petclinic.model.Owner;
+
+/**
+ * created by @matthewgitata on 19/03/2023
+ */
+public interface OwnerRepository extends CrudRepository<Owner, Long>{
+}
---
+++ petclinic/pet-clinic-data/src/main/java/com/matthewgitata/petclinic/repositories/SpecialtyRepository.java
@@ -0,0 +1,9 @@
+package com.matthewgitata.petclinic.repositories;
+
+import com.matthewgitata.petclinic.model.Specialty;
+
+/**
+ * created by @matthewgitata on 19/03/2023
+ */
+public interface SpecialtyRepository extends CrudRepository<Specialty, Long> {
+}
