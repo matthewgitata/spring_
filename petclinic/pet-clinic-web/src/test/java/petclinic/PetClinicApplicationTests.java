ae480ddf684a103991afe9cc84ee7c5db217b701
Matthew Gitata <matthewgitata@gmail.com>
2023-03-20 00:00:00 +0300

    Upgrade to JUnit 5.

--- petclinic/pet-clinic-web/src/test/java/petclinic/PetClinicApplicationTests.java
+++ petclinic/pet-clinic-web/src/test/java/petclinic/PetClinicApplicationTests.java
@@ -3,6 +3,7 @@ package petclinic;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class PetClinicApplicationTests {
 
