19c6d581bb397e1e7721dd65acd2398ab9bfa5a0
Matthew Gitata <matthewgitata@gmail.com>
2023-03-20 13:02:00 +0300

    Display remaining recipe properties.

--- springrecipeapp/src/main/java/com/matthewgitata/recipeapp/bootstrap/RecipeBootstrap.java
+++ springrecipeapp/src/main/java/com/matthewgitata/recipeapp/bootstrap/RecipeBootstrap.java
@@ -137,6 +137,10 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
         guacamoleRecipe.getCategories().add(americanCategory);
         guacamoleRecipe.getCategories().add(mexicanCategory);
 
+        guacamoleRecipe.setUrl("http://www.simplyrecipes.com/recipes/perfect_guacamole/");
+        guacamoleRecipe.setServings(4);
+        guacamoleRecipe.setSource("Simply Recipes");
+
         // add to return list
         recipes.add(guacamoleRecipe);
 
