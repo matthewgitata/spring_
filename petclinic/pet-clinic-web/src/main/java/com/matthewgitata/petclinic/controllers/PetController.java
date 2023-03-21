0fc3ef1e2e8c18100641dae7968f67d62fca8340
Matthew Gitata <matthewgitata@gmail.com>
2023-03-21 13:33:33 +0300

    Add ModelAttribute for PetController.

---
+++ petclinic/pet-clinic-web/src/main/java/com/matthewgitata/petclinic/controllers/PetController.java
@@ -0,0 +1,37 @@
+package com.matthewgitata.petclinic.controllers;
+
+import java.util.Collection;
+
+/**
+ * created by @matthewgitata on 21/03/2023
+ */
+@Controller
+@RequestMapping("/owners/{ownerId}")
+public class PetController {
+
+    private static final String VIEWS_PETS_CREATE_OR_UPDATE_FORM = "pets/createOrUpdatePetForm";
+    private final PetService petService;
+    private final OwnerService ownerService;
+    private final PetTypeService petTypeService;
+
+    public PetController(PetService petService, OwnerService ownerService, PetTypeService petTypeService) {
+        this.petService = petService;
+        this.ownerService = ownerService;
+        this.petTypeService = petTypeService;
+    }
+
+    @ModelAttribute("types")
+    public Collection<PetType> populatePetTypes() {
+        return petTypeService.findAll();
+    }
+
+    @ModelAttribute("owner")
+    public Owner findOwner(@PathVariable("ownerId") Long ownerId) {
+        return ownerService.findById(ownerId);
+    }
+
+    @InitBinder("owner")
+    public void initOwnerBinder(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+}
