240eca6c4ef85937a3f79584cb2f1aa5e304bd2c
Matthew Gitata <matthewgitata@gmail.com>
2023-03-22 20:40:00 +0300

    Create Data Model.

---
+++ springrestclient/src/main/java/com/matthewgitata/api/domain/Billing.java
@@ -0,0 +1,7 @@
+package com.matthewgitata.api.domain;
+
+/**
+ * created by @matthewgitata on 22/03/2023.
+ */
+public class Billing {
+}
---
+++ springrestclient/src/main/java/com/matthewgitata/api/domain/UserData.java
@@ -0,0 +1,16 @@
+package com.matthewgitata.api.domain;
+
+/**
+ * created by @matthewgitata on 22/03/2023.
+ */
+public class UserData {
+    List<User> data;
+
+    public List<User> getData() {
+        return data;
+    }
+
+    public void setData(List<User> data) {
+        this.data = data;
+    }
+}
