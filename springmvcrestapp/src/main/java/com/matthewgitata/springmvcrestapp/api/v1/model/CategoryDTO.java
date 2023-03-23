a95b2e7cfce767044d2e11bae627da09bd6bdaad
Matthew Gitata <matthewgitata@gmail.com>
2023-03-23 08:14:00 +0300

    Set up MapStruct Library.

--- springmvcrestapp/src/main/java/com/matthewgitata/springmvcrestapp/api/v1/model/CategoryDTO.java
+++ springmvcrestapp/src/main/java/com/matthewgitata/springmvcrestapp/api/v1/model/CategoryDTO.java
@@ -3,6 +3,7 @@ package com.matthewgitata.springmvcrestapp.api.v1.model;
 /**
  * created by @matthewgitata on 23/03/2023.
  */
+@Data
 public class CategoryDTO {
     private Long id;
     private String name;
