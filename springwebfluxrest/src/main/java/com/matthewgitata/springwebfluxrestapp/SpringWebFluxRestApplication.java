5ec36a387b20feb5480d61f3fe44a3e02e34b103
Matthew Gitata <matthewgitata@gmail.com>
2023-03-24 07:22:00 +0300

    Start: Initial project add from Spring Initializr.

---
+++ springwebfluxrest/src/main/java/com/matthewgitata/springwebfluxrestapp/SpringWebFluxRestApplication.java
@@ -0,0 +1,13 @@
+package com.matthewgitata.springwebfluxrestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebFluxRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebFluxRestApplication.class, args);
+	}
+
+}
